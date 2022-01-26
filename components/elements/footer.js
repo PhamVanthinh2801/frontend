2083abd832f8d690b6183fc25b91c8978aeb92e2
Pham Thinh <thinhphamndtdtu@gmail.com>
2022-01-26 12:22:47 +0700

    up

--- components/elements/footer.js
+++ components/elements/footer.js
@@ -22,8 +22,8 @@ const Footer = () => {
           </p>
         </Link>
 
-        <p className='text-[#000000] text-center text-2xl font-medium pb-2 cursor-pointer'>
-          Phone: 098 394 79 69
+        <p className='text-[#000000] text-center text-2xl font-medium pb-2 cursor-pointer  hover:text-red-700'>
+          <a href='tel:+0983947969'>Phone: 098 394 79 69</a>
         </p>
       </div>
 
@@ -31,30 +31,32 @@ const Footer = () => {
         <div className='m-2  h-8 w-8 cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'>
           <Link href='https://www.facebook.com/vietkatv/'>
             <img
-              src='https://res.cloudinary.com/hiname/image/upload/v1642658891/7_pyjclz.jpg'
-              width={54}
-              height={54}
-              style={{ borderRadius: "12px" }}
+              src='https://res.cloudinary.com/hiname/image/upload/v1643172052/logo-facebook_xwiukt.png'
+              width={74}
+              height={74}
+              style={{ borderRadius: "22px" }}
             />
           </Link>
         </div>
         <div className='m-2 h-8 w-8 cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'>
           <Link href='https://www.youtube.com/c/VietKATV'>
             <img
-              src='https://res.cloudinary.com/hiname/image/upload/v1642658773/6_o4xviq.png'
-              width={54}
-              height={54}
-              style={{ borderRadius: "12px" }}
+              src='https://res.cloudinary.com/hiname/image/upload/v1643172052/youtobe_xmfi1o.png'
+              width={74}
+              height={74}
+              style={{ borderRadius: "22px" }}
             />
           </Link>
         </div>
         <div className='m-2 h-8 w-8 cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'>
-          <img
-            src='https://res.cloudinary.com/hiname/image/upload/v1642658658/4_uxa405.jpg'
-            width={54}
-            height={54}
-            style={{ borderRadius: "12px" }}
-          />
+          <Link href={`http://zalo.me/0983947969`}>
+            <img
+              src='https://res.cloudinary.com/hiname/image/upload/v1643172053/zalo_t7xiir.png'
+              width={74}
+              height={74}
+              style={{ borderRadius: "22px" }}
+            />
+          </Link>
         </div>
       </div>
     </div>
